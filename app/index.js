4284a4e0477cdaa361b9cfa1b01dbaae6bc5bac1
Manuel Aguilar <manuelmla.aguilar@gmail.com>
2021-09-21 21:55:03 -0300

    Added Sqlite3 db

--- app/index.js
+++ app/index.js
@@ -1,16 +1,54 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import MatchHistory from '../match-history/index.js';
+import fs from 'fs';
+import sqlite3 from 'sqlite3';
+sqlite3.verbose();
 
+const app = express();
+app.use(bodyParser.urlencoded(({extended: true})));
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
-const app = express();
 const matchHistory = new MatchHistory();
 
 app.use(bodyParser.json());
 
 //Add persist layer
+//init sqlite debugger
+const dbfile = './SqLiteDb';
+const exists = fs.existsSync(dbfile);
+const db = new sqlite3.Database(dbfile, (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+});
+
+// if !exists = create
+db.serialize(() => {
+    if(!exists) {
+        db.run(
+            "CREATE TABLE Matches (id INTEGER PRIMARY KEY AUTOINCREMENT, result INTEGER)"
+        );
+        console.log("Created db, matches table");
+
+        db.serialize(() => {
+            db.run (
+                "INSERT INTO Matches (result) VALUES (1),(2),(2),(3),(1)"
+            );
+        });
+    } 
+    console.log("Database Matches ready to go");
+    db.each("SELECT * from Matches", (err, row) => {
+        if(err){
+            console.error(err.message);
+        }
+        if(row) {
+            console.log(`record: ${row.result}`);
+        }
+    });
+});
+
 //Calculate dmgs
 //Use matches data
 //Maybe get W/D/L
