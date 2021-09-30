0fc16e151eefdc049235bb80730e27e9fb8901d2
Manuel Aguilar <manuelmla.aguilar@gmail.com>
2021-09-29 21:28:28 -0300

    Added sequelize support

--- data/index.js
+++ data/index.js
@@ -1,40 +1,54 @@
 import fs from 'fs';
 import sqlite3 from 'sqlite3';
+import Sequelize from 'sequelize';
 sqlite3.verbose();
 
 //Add persist layer
 class Data{
     constructor() {
-        const dbfile = './SqLiteDb';
-        this.exists = fs.existsSync(dbfile);
-        this.db = new sqlite3.Database(dbfile, (err) => {
-            if (err) {
-                console.error(err.message);
-            }
-        });
-        this.initializeDatabase();
+        this.connString = 'sqlite:SqLiteDb';
+        // this.exists = fs.existsSync(dbfile);
+        // this.db = new sqlite3.Database(dbfile, (err) => {
+        //     if (err) {
+        //         console.error(err.message);
+        //     }
+        // });
+        // this.initializeDatabase();
+    }
+
+
+
+    async testConnection(){
+        try {
+            const sequelize = new Sequelize(this.connString);
+            await sequelize.authenticate();
+            console.log('Connection has been established succesfully.');
+            sequelize.close();
+        } catch(error) {
+            console.error('Unable to connect to the database.', error);
+        }
     }
     //init sqlite debugger
     initializeDatabase(){
         // if !exists = create
-        this.db.serialize(() => {
-            if(! this.exists) {
-                this.db.run(
-                    "CREATE TABLE Matches (id INTEGER PRIMARY KEY AUTOINCREMENT, result INTEGER, date DATETIME, slpGain INTEGER)"
-                );
-                console.log("Created Matches table");
-                this.db.run(
-                    "CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, slpAmount INTEGER, cups INTEGER)"
-                );
-                console.log("Created Users table");
+        // this.db.serialize(() => {
+        //     if(! this.exists) {
+        //         this.db.run(
+        //             "CREATE TABLE Matches (id INTEGER PRIMARY KEY AUTOINCREMENT, result INTEGER, date DATETIME, slpGain INTEGER)"
+        //         );
+        //         console.log("Created Matches table");
+        //         this.db.run(
+        //             "CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, slpAmount INTEGER, cups INTEGER)"
+        //         );
+        //         console.log("Created Users table");
 
-                this.db.serialize(() => {
-                    this.db.run (
-                        "INSERT INTO Matches (result) VALUES (1),(2),(2),(3),(1)"
-                    );
-                });
-            } 
-        });
+        //         this.db.serialize(() => {
+        //             this.db.run (
+        //                 "INSERT INTO Matches (result) VALUES (1),(2),(2),(3),(1)"
+        //             );
+        //         });
+        //     } 
+        // });
     }
 }
 
